a010a53ab5f9dc62f2bde5a00b4574864dd46fad
Sukhjinder Singh <sukhjindercheema1@gmail.com>
2024-01-14 20:51:10 -0500

    Tesing phase

---
+++ student.py
@@ -0,0 +1,9 @@
+class game:
+  def __init__(self, promt, answer):
+    self.promt = promt
+    self.answer = answer
+
+p1 = game("John", 36)
+
+print(p1.promt)
+print(p1.answer)
---
+++ test.py
@@ -0,0 +1,17 @@
+
+class chef:
+
+    def makechicken(self):
+        print("make chicken yourself")
+
+    def makesalad(self):
+        print("make slad")
+
+    def makespecialdish(self):
+        print("make special tea")
+
+
+
+calling=chef()
+
+calling.makechicken()
